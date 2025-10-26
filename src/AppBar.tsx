cd0b3ae1a68dd07b6862e06eeff4b6eefb8a63df
Utsav Barnwal <u.barnwal36@gmail.com>
2025-10-26 16:48:36 +0700

    Refactor project structure and update dependencies

--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-
+import { useState } from 'react';
 import Icon from './assets/icons/Icon-Electron.png';
 
 function AppBar() {
@@ -15,34 +14,23 @@ function AppBar() {
   };
 
   return (
-    <>
-      <div className="bg-slate-800 py-0.5 flex justify-between draggable text-white">
-        <div className="inline-flex">
-          <img className="h-6 lg:-ml-2" src={Icon} alt="Icon of Electron" />
-          <p className="text-xs md:pt-1 md:-ml-1 lg:-ml-2">Vite App</p>
-        </div>
-        <div className="inline-flex -mt-1">
-          <button onClick={window.Main.Minimize} className="undraggable md:px-4 lg:px-3 pt-1 hover:bg-gray-300">
-            &#8211;
-          </button>
-          <button onClick={handleToggle} className="undraggable px-6 lg:px-5 pt-1 hover:bg-gray-300">
-            {isMaximize ? '\u2752' : '⃞'}
-          </button>
-          <button onClick={window.Main.Close} className="undraggable px-4 pt-1 hover:bg-red-500 hover:text-white">
-            &#10005;
-          </button>
-        </div>
+    <div className="bg-slate-800 py-0.5 flex justify-between draggable text-white">
+      <div className="inline-flex">
+        <img className="h-6 lg:-ml-2" src={Icon} alt="Icon of Electron" />
+        <p className="text-xs md:pt-1 md:-ml-1 lg:-ml-2">Vite App</p>
       </div>
-      <div className="bg-slate-800 text-white undraggable">
-        <div className="flex text-center">
-          <div className="text-sm w-8  hover:bg-gray-700">File</div>
-          <div className="text-sm w-8   hover:bg-gray-700">Edit</div>
-          <div className="text-sm w-10  hover:bg-gray-700">View</div>
-          <div className="text-sm w-14  hover:bg-gray-700 ">Window</div>
-          <div className="text-sm w-9  hover:bg-gray-700 ">Help</div>
-        </div>
+      <div className="inline-flex -mt-1">
+        <button onClick={window.Main.Minimize} className="undraggable md:px-4 lg:px-3 pt-1 hover:bg-gray-300">
+          &#8211;
+        </button>
+        <button onClick={handleToggle} className="undraggable px-6 lg:px-5 pt-1 hover:bg-gray-300">
+          {isMaximize ? '\u2752' : '⃞'}
+        </button>
+        <button onClick={window.Main.Close} className="undraggable px-4 pt-1 hover:bg-red-500 hover:text-white">
+          &#10005;
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
